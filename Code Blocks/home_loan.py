8e96c4e9da6cce332887bba7a542d5dcd9216e19
Sam Roberts <sam.roberts1995@gmail.com>
2022-07-27 17:56:23 +1000

    Update File Structure and Sam/Ryans Code

--- Code Blocks/home_loan.py
+++ Code Blocks/home_loan.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from forex_python.converter import CurrencyRates
 import numpy_financial as npf
+import requests
 
 #Vinay added for geo location calculation using post code. So before running the code pip install pgeocode in dev environment.
 import pgeocode
@@ -70,7 +71,7 @@ if currency_option != 'AUD':
         else:
             st.sidebar.write(f"Current Savings Available: {round(savings_aud)} AUD")
 
-
+# Start of Sams Code
 st.sidebar.write("# Loan Calculator")
 
 year_option = st.sidebar.selectbox('Term of Loan',('Select Loan Term','5', '10', '20', '30'))
@@ -80,6 +81,8 @@ savings_aud = int(savings) * c.get_rate(currency_option, 'AUD')
 
 int_rate = 0.06
 pmts_year = 12
+
+#End of Sams Code
 
 if st.sidebar.button("Determine Loan Amount"):
     # Vinay's code starts here
@@ -96,6 +99,7 @@ if st.sidebar.button("Determine Loan Amount"):
 
     # Vinay's code ends here
 
+    #Sam's Code:
     #loan_amount = npf.pv(int_rate/pmts_year, int(year_option)*pmts_year, (savings_aud) * -1)
     st.session_state['loan_amount'] = npf.pv(int_rate/pmts_year, int(year_option)*pmts_year, (savings_aud) * -1)
     st.write("With a payment of ${:,.0f} per month,".format(round(savings_aud)))
@@ -107,8 +111,54 @@ if st.sidebar.button("Determine Loan Amount"):
 
     with tab1:
         st.header("Geolocation")
-        st.write("LINK GEOLOCATION WITH MEDIAN HOUSE PRICE & POST CODE")
-    
+
+        df = fetch_geo_data()
+        df = df[df.Price <= st.session_state['loan_amount']]
+
+        #st.dataframe(select_df)
+
+        # Show State by Lat and Lon
+        st.pydeck_chart(pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+        initial_view_state=pdk.ViewState(
+         latitude=-37.8136,
+         longitude=144.946457,
+         zoom=11,
+         pitch=50,
+        ),
+
+        layers=[
+        # Add in Layer 1
+         pdk.Layer(
+            'HexagonLayer',
+            data=df,
+            get_position='[longitude, latitude]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+         ),
+
+         # Add in Layer 2
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=df,
+             get_position='[longitude, latitude]',
+             get_color='[200, 30, 0, 160]',
+             get_radius=200,
+         ),
+     ],
+    ))
+        my_expander = st.expander(label='Expand me')
+        with my_expander:
+            st.write("Raw Data For Additional Information")
+            cm = sns.light_palette("green", as_cmap=True)
+            st.dataframe(df.style.background_gradient(cmap=cm).set_precision(2))
+        
+    #End of Sams Code
+
+
     with tab2:
         tab2.header("Dwellings Suggestions")
         tab2.write("Suggestions for best postcode to invest in")
@@ -130,7 +180,78 @@ if st.sidebar.button("Determine Loan Amount"):
 
     with tab3:
         st.header("Housing Market Sentiments")
-        st.write("Housing NEWS")
+        news_from_date = "2022-06-27"
+
+        api_key='8e935984687a40f79f1adf1d3ebfd548'
+        url = "https://newsapi.org/v2/everything?q=melbourne-house-prices&from="+news_from_date+"&apiKey="+api_key
+        news = requests.get(url).json()
+
+        news_article = list(range(0,10))
+
+        news_raw = []
+        a = news["articles"]
+
+        for each_news in news_article:
+            news_raw.append(a[each_news])
+
+        title = []
+        description = []
+        date = []
+        date_format=[]
+
+        for i in news_article:
+            title.append(news_raw[i]['title'])
+            description.append(news_raw[i]['description'])
+            date.append(news_raw[i]['publishedAt'])
+
+        #change date format
+        for j in range(0,len(date)):
+            date_format.append(date[j][0:10])
+
+        news = {"Published_Date": date_format,"News_Headline": title, "News_Content": description}
+
+        news_df = pd.DataFrame(data=news)
+        st.dataframe(news_df)
+        
+        # Instantiate the lemmatizer
+        lemmatizer = WordNetLemmatizer()
+
+        # Create a list of stopwords
+        sw = set(stopwords.words('english'))
+
+        def tokenizer(text):
+            """Tokenizes text."""
+        
+            # Create a list of the words
+            sw = set(stopwords.words('english'))
+            # Convert the words to lowercase
+            regex = re.compile("[^a-zA-Z ]")
+            # Remove the punctuation
+            re_clean = regex.sub('', text)
+            # Remove the stop words
+            words = word_tokenize(re_clean)
+            # Lemmatize Words into root words
+            lem = [lemmatizer.lemmatize(word) for word in words]
+            tokens = [word.lower() for word in lem if word.lower() not in sw]
+        
+            #return non list tokens so word cloud would work
+            return ' '.join([str(tokens) for element in tokens])
+        
+        news_df['headline_tokens'] = news_df['News_Headline'].apply(tokenizer)
+
+        from wordcloud import WordCloud
+        import matplotlib.pyplot as plt
+        plt.style.use('seaborn-whitegrid')
+        import matplotlib as mpl
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        
+        mpl.rcParams['figure.figsize'] = [20.0, 10.0]
+
+        news_text = ' '.join(news_df.headline_tokens)
+        wc = WordCloud().generate(news_text)
+        plt.imshow(wc)
+        plt.show()
+        st.pyplot()
 
 # Vinay's code starts here for clearing the session state to enable fresh calculation    
 for key in st.session_state.keys():
